f415c5ff0fa157004c56bf36dafe45d62109ce3a
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2020-07-14 08:10:11 +0700

    Fragment Pembelian

---
+++ app/src/main/java/project/manajemenstok/ui/main/fragment/FragmentPembelian.kt
@@ -0,0 +1,21 @@
+package project.manajemenstok.ui.main.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import project.manajemenstok.R
+
+
+class FragmentPembelian : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        return inflater!!.inflate(R.layout.fragment_pembelian, container, false)
+    }
+
+}
--- app/src/main/java/project/manajemenstok/ui/main/view/PembelianActivity.kt
+++ app/src/main/java/project/manajemenstok/ui/main/view/PembelianActivity.kt
@@ -4,14 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_pembelian.*
 import project.manajemenstok.R
+import project.manajemenstok.ui.main.adapter.PembelianAdapter
 
 class PembelianActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pembelian)
+
+        val fragmentAdapter = PembelianAdapter(supportFragmentManager)
+        val tes : ViewPager = findViewById(R.id.viewpager_main)
+        tes.adapter = fragmentAdapter
+
+        val tabs : TabLayout = findViewById(R.id.tabs_main)
+
+        tabs.setupWithViewPager(tes)
     }
 
     override fun onClick(v: View) {
